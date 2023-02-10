c4e497e382e4bfad998b46fb7cd070ab5c60f7ac
AA00163 <AAlimohammadi00163@gmail.com>
2023-02-10 00:47:20 -0800

    permission_admin

--- orders/admin.py
+++ orders/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Order, OrderItems
 
 
-# admin.register(Order)(admin.ModelAdmin)
-# admin.register(OrderItems)(admin.ModelAdmin)
+admin.register(Order)(admin.ModelAdmin)
+admin.register(OrderItems)(admin.ModelAdmin)
