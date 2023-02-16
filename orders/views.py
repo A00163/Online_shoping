2c74d64330b26b0d898087478479e916bfc9fe8d
AA00163 <AAlimohammadi00163@gmail.com>
2023-02-15 16:42:10 -0800

    add_coupon

--- orders/views.py
+++ orders/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from product.models import Product
 from .cart import Cart
-from .forms import CartAddForm
+from .forms import CartAddForm, CouponForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Order, OrderItems
-
+from .models import Order, OrderItems, Coupon
+import datetime
+from django.contrib import messages
 
 class CartView(View):
     template_name = 'orders/cart.html'
@@ -35,10 +36,11 @@ class CartRemoveView(View):
 
 class OrderDetailView(LoginRequiredMixin, View):
     template_name = 'orders/order.html'
+    form = CouponForm
 
     def get(self, request, order_id):
         order = get_object_or_404(Order, id=order_id)
-        return render(request, self.template_name, {'order': order})
+        return render(request, self.template_name, {'order': order, 'form': self.form})
 
 
 class OrderCreateView(LoginRequiredMixin, View):
@@ -52,3 +54,24 @@ class OrderCreateView(LoginRequiredMixin, View):
                                       quantity=item['quantity'])
         cart.clear()
         return redirect(self.template_name, order.id)
+
+
+class CouponView(LoginRequiredMixin, View):
+    form = CouponForm
+
+    def post(self, request, order_id):
+        now = datetime.datetime.now()
+        form = self.form(request.POST)
+        if form.is_valid():
+            code = form.cleaned_data['code']
+            try:
+                coupon = Coupon.objects.get(code__exact=code, valid_from__lte=now, valid_to__gte=now, active=True)
+            except Coupon.DoesNotExist:
+                messages.error(request, 'this coupon does not exist', 'danger')
+                return redirect('orders:order_detail', order_id)
+            order = Order.objects.get(id=order_id)
+            order.discount = coupon.discount
+            order.save()
+        return redirect('orders:order_detail', order_id)
+
+
